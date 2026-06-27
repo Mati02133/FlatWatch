60f4b2af438b231dd3f103c44b4dfbbf0a8f22b7
Mateusz Jasek <mateuszjasek28@gmail.com>
2026-06-28 01:59:03 +0200

    fix: correct region and city IDs for Krakow in OLX config

--- backend/config.py
+++ backend/config.py
@@ -30,10 +30,13 @@ SERVICES = { # Strony z ktorych bedziemy korzystac
 OLX_CATEGORY_ID = 15 # 15 to mieszkania
 
 OLX_REGIONS = {
-    "Krakow" : 8,
+    "Krakow" : 4,
     "Warszawa" : 7,
     "Wroclaw" : 9,
 
+}
+OLX_CITIES = {
+    "Krakow": "8959",
 }
 
 INTERVAL_MINUTES = 30 # co ile minut bedzie sprawdzac nowe ogloszenia
