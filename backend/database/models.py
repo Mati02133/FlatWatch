4576d9a6deffdf9bd1828f647e04ca9f1a55e7f0
Mateusz Jasek <mateuszjasek28@gmail.com>
2026-06-30 01:19:57 +0200

    feat: add database models with offers table schema

---
+++ backend/database/models.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+from config import DATABASE_URL
+
+DB_PATH = DATABASE_URL.replace("sqlite:///", "")
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH) 
+    conn.row_factory = sqlite3.Row # ustawiamy row_factory aby zwracac wiersze wedlug nazw kolumn
+    return conn
+
+def create_tables():
+    conn = get_connection()
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS offers (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            external_id TEXT UNIQUE NOT NULL,
+            service     TEST NOT NULL,
+            title       TEXT,
+            description TEXT,
+            price       INTEGER,
+            area        TEXT,
+            rooms       TEXT,
+            city        TEXT,
+            url         TEXT,
+            is_private  INTEGER DEAFULT 1,
+            is_active   INTEGER DEAFULT 1,
+            created_at  TEXT DEAFULT (datetime("now"))
+        )
+    """)
+
+    conn.commit() 
+    conn.close()
+    print("DATABASE CREATED")
